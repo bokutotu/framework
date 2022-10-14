8abcfa592bf7b3fb00f9aa1792785d84ae185f2a
bokutotu <mushin.hudoushin@gmail.com>
2022-10-14 22:18:45 +0900

    add test for shpae

--- src/tensor.rs
+++ src/tensor.rs
@@ -13,22 +13,26 @@ impl Shape {
         for i in 0..shape.len()-1 {
             res.push(res[i] * shape[i]);
         }
+        res.reverse();
         Stride::new(res)
+    }
+
+    fn is_default_stride(&self, stride: &Stride) -> bool {
+        self.default_stride() == *stride
     }
 
     fn num_elms(&self) -> usize {
         self.0.iter().fold(1, |x, y| { x * y })
     }
 
-    fn valid_index(&self, index: &Index) {
+    fn valid_index(&self, index: &TensorIndex) {
         if self.0.len() != index.0.len() {
             panic!("This tensor is {} dimensions but your index is {} dimensions", 
                    self.0.len(), index.0.len());
         }
-        let mut flag = true;
-        index.iter().zip(self.0.iter())
-            .for_each(|(idx, shpae)| { 
-                if flag == (idx < shpae) {
+        index.0.iter().zip(self.0.iter())
+            .for_each(|(idx, shape)| { 
+                if idx >= shape {
                     panic!("this tensor's shape is {} but your index is {}",
                            idx, shape)
                 }
@@ -45,18 +49,20 @@ impl Stride {
     }
 
     // must use before valid_index
-    fn cal_offset(&self, index: &Index) -> usize {
-
+    fn cal_offset(&self, index: &TensorIndex) -> usize {
+        self.0.iter()
+            .zip(index.0.iter())
+            .fold(1, |offset, (dim_index, dim_stride)| offset + dim_index * dim_stride)
     }
 }
 
-pub(crate) struct Inner<T> {
+pub(crate) struct OwnedInner<T> {
     pointer: *mut T,
     shape: Shape,
     stride: Stride,
 }
 
-impl<T> Inner<T> {
+impl<T> OwnedInner<T> {
     fn shape(&self) -> Shape {
         self.shape.clone()
     }
@@ -67,33 +73,23 @@ impl<T> Inner<T> {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub Index ( Vec<usize> );
+pub struct TensorIndex ( Vec<usize> );
 
-pub(crate) struct CPUInner<T> ( Inner<T> );
-pub(crate) struct GPUInner<T> ( Inner<T> );
+pub(crate) struct CPUInner<T> ( OwnedInner<T> );
+pub(crate) struct GPUInner<T> ( OwnedInner<T> );
 
 impl<T> CPUInner<T> {
     fn cpu_malloc(shape: Shape, stride: Stride) -> Self{
-        shape.check_stride(&stride);
         let alloc_vec: Vec::<T> = Vec::with_capacity(shape.num_elms());
         let pointer: *mut T = alloc_vec.as_ptr() as *mut T;
         std::mem::forget(alloc_vec);
-        CPUInner(Inner {pointer, shape, stride } )
-    }
-
-    fn cpu_cal_offset(&self, index: &Index) -> Self {
-
+        CPUInner(OwnedInner {pointer, shape, stride } )
     }
 }
 
 impl<T> GPUInner<T> {
     fn gpu_malloc(shape: Shape, stride: Stride) -> Self {
-        shape.check_stride(&stride);
         todo!()
-    }
-
-    fn gpu_cal_offset(index: &Index) -> usize {
-        todo!();
     }
 }
 
@@ -127,8 +123,13 @@ impl<T> Pointer<T> {
         Pointer::GPU(pointer)
     }
 
-    fn cal_index_from_shape(index: Index) -> usize {
-        
+    fn cal_offset(self, index: TensorIndex) -> usize {
+        self.shape().clone().valid_index(&index);
+        self.stride().clone().cal_offset(&index)
+    }
+
+    fn is_default_stride(&self) -> bool {
+        self.shape().default_stride().0 == self.stride().0
     }
 }
 
@@ -159,3 +160,87 @@ impl<T> Tensor<T> {
         Tensor { inner: pointer }
     }
 }
+
+macro_rules! impl_defalut_stride_test {
+    ($fn_name:ident, $vec:expr, $ans:expr) => {
+        #[test]
+        fn $fn_name() {
+            let shape = Shape::new($vec);
+            let default_stride = shape.default_stride();
+            assert_eq!(default_stride.0, $ans)
+        }
+    };
+}
+
+impl_defalut_stride_test!(test_default_stride_1d, vec![10], vec![1]);
+impl_defalut_stride_test!(test_default_stride_2d, vec![2, 3], vec![3, 1]);
+impl_defalut_stride_test!(test_default_stride_3d, vec![2,3,4], vec![12, 4, 1]);
+
+macro_rules! impl_is_default_stride_test {
+    (@inner $shape:ident, $stride:ident, $shape_vec:expr, $stride_vec:expr) => {
+        let $shape = Shape::new($shape_vec);
+        let $stride = Stride::new($stride_vec);
+    };
+
+    (@eq $fn_name:ident, $shape_vec:expr, $stride_vec:expr) => {
+        #[test] 
+        fn $fn_name() {
+            impl_is_default_stride_test!(@inner shape, stride, $shape_vec, $stride_vec);
+            assert!(shape.is_default_stride(&stride));
+        }
+    };
+
+    (@nq $fn_name:ident, $shape_vec:expr, $stride_vec:expr) => {
+        #[test]
+        fn $fn_name() {
+            impl_is_default_stride_test!(@inner shape, stride, $shape_vec, $stride_vec);
+            assert!(!shape.is_default_stride(&stride));
+        }
+    };
+}
+
+impl_is_default_stride_test!(@eq test_default_stride_1d_eq, vec![10], vec![1]);
+impl_is_default_stride_test!(@nq test_default_stride_1d_nq, vec![10], vec![2]);
+impl_is_default_stride_test!(@eq test_default_stride_2d_eq, vec![2, 3], vec![3,1]);
+impl_is_default_stride_test!(@nq test_default_stride_2d_nq, vec![2, 3], vec![1,1]);
+impl_is_default_stride_test!(@eq test_default_stride_3d_eq, vec![4, 2, 3], vec![6, 3,1]);
+impl_is_default_stride_test!(@nq test_default_stride_3d_nq, vec![4, 2, 3], vec![5, 1,1]);
+
+macro_rules! impl_num_elm_test {
+    ($fn_name:ident, $shape:expr, $ans:expr) => {
+        #[test]
+        fn $fn_name() {
+            let shape = Shape::new($shape);
+            assert_eq!(shape.num_elms(), $ans);
+        }
+    };
+}
+
+impl_num_elm_test!(num_elm_1d, vec![2], 2);
+impl_num_elm_test!(num_elm_2d, vec![3, 2], 6);
+impl_num_elm_test!(num_elm_3d, vec![1, 2, 2], 4);
+impl_num_elm_test!(num_elm_4d, vec![2, 3, 4, 5], 120);
+
+macro_rules! impl_valid_index_test {
+    (@should_panic $fn_name:ident, $shape:expr, $index:expr) => {
+        impl_valid_index_test!(@impl_fn $fn_name, $shape, $index, should_panic, test);
+    };
+
+    (@should_success $fn_name:ident, $shape:expr, $index: expr) => {
+        impl_valid_index_test!(@impl_fn $fn_name, $shape, $index, test);
+    };
+
+    (@impl_fn $fn_name:ident, $shape:expr, $index:expr, $($meta:meta),*) => {
+        $(
+            #[$meta]
+        )*
+        fn $fn_name() {
+            let shape = Shape::new($shape);
+            let index = TensorIndex ( $index );
+            shape.valid_index(&index);
+        }
+    };
+}
+
+impl_valid_index_test!(@should_success valid_index_1d, vec![10], vec![3]);
+impl_valid_index_test!(@should_panic valid_index_1d_panic, vec![10], vec![10]);
