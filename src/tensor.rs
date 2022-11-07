eecf76161851b532e0c08a7b7e2fa3f7a60843db
bokutotu <mushin.hudoushin@gmail.com>
2022-11-07 21:28:16 +0900

    add test for to_owned and to_vec for arraybase

--- src/tensor.rs
+++ src/tensor.rs
@@ -15,3 +15,13 @@ where
 pub type CpuTensor<E> = TensorBase<OwnedCpu<E>, E>;
 pub type CpuViewTensor<E> = TensorBase<ViewCpu<E>, E>;
 pub type CpuViewMutTensor<E> = TensorBase<ViewMutCpu<E>, E>;
+
+#[test]
+fn to_view_to_onwend_test() {
+    use crate::shape::Shape;
+    let from_vec = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+    let from_array = CpuTensor::from_vec(from_vec.clone(), Shape::new(vec![3, 3]));
+    let view = from_array.to_view();
+    let to_owned = view.into_owned();
+    assert_eq!(from_vec, to_owned.to_vec());
+}
